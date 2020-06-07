c73c74b163e8560e63437d53cb3d8bdb999a2d9e
jeSuisCodeuse <creativegirl@outlook.fr>
2020-06-07 22:05:56 +0200

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+const fill = document.querySelector('.fill');
+const empty = docoment.querySelectorAll('.empty');
+
+fill.addEventListener('dragstart', dragStart());
+fill.addEventListener('dragend', dragEndtart());
+
+function dragStart() {
+   
+}
+
+function dragEnd() {
+
+}
