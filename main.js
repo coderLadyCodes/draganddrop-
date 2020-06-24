07bf5fe1b70a7536031ef1da4ac4ecdd33427af2
jeSuisCodeuse <creativegirl@outlook.fr>
2020-06-24 16:03:16 +0200

    w'll see

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
-let list = document.querySelectorAll('.list');
-let item = document.querySelectorAll('.items');
+const lists = document.querySelectorAll('.list');
+const items = document.querySelectorAll('.items');
 
+let draggedItem = null;
+
+for(let i = 0; i < items.length; i++) {
+     const item = items[i];
+
+item.addEventListener('dragstart', function() {
+      draggedItem = item;
+      setTimeout(function() {
+           item.style.display = 'none';
+      }, 0)
+});
+
+item.addEventListener('dragend', function() {
+     setTimeout(function() {
+          draggedItem.style.display = 'block';
+          draggedItem = null;
+     }, 0)
+});
+
+for(let l = 0; l < lists.length; l++) {
+     const list = lists[l];
+
+list.addEventListener('dragover', function(e) {
+       e.preventDefault();
+       
+});
+
+list.addEventListener('dragenter', function(e) {
+       e.preventDefault();
+       
+});
+
+list.addEventListener('drop', function() {
+     
+     this.append(draggedItem);
+     
+     
+     
+});
+}
+}
